2312520eb50d64c2bc2a259af15916298ccc66db
Daniel Antonio Ceballos Flores <danielc@linqstream.com>
2020-07-21 14:15:04 -0400

    Finish basic about

--- src/views/About.js
+++ src/views/About.js
@@ -6,6 +6,7 @@ import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import silvana from "../assets/silvana.jpg";
 import daniel from "../assets/daniel.jpg";
+import both from "../assets/both.jpg";
 
 const useStyles = createUseStyles({
   myContainer: {
@@ -18,14 +19,26 @@ const useStyles = createUseStyles({
     border: "solid",
   },
   cardImage: {
-      maxHeight: '50vh'
+    maxHeight: "50vh",
   },
   myCard: {
-      boxShadow: '5px 4px 8px 0 rgba(0,0,0,0.2)',
-      transition: '0.3s',
-      '&:hover': {
-        boxShadow: '10px 8px 16px 0 rgba(0,0,0,0.2)'
-      }
+    boxShadow: "5px 4px 8px 0 rgba(0,0,0,0.2)",
+    transition: "0.3s",
+    "&:hover": {
+      boxShadow: "10px 8px 16px 0 rgba(0,0,0,0.2)",
+    },
+  },
+  usCard: {
+    boxShadow: "5px 4px 8px 0 rgba(0,0,0,0.2)",
+    transition: "0.3s",
+    "&:hover": {
+      boxShadow: "10px 8px 16px 0 rgba(0,0,0,0.2)",
+    },
+  },
+  '@media (min-width: 727px)': {
+    usCard: {
+      maxWidth: '30vw'
+    }
   }
 });
 
@@ -36,7 +49,11 @@ const About = () => {
       <Row>
         <Col sm={12} md={6} lg={6} className={classes.myCol}>
           <Card className={classes.myCard} fluid>
-            <Card.Img className={classes.cardImage} variant="top" src={silvana} />
+            <Card.Img
+              className={classes.cardImage}
+              variant="top"
+              src={silvana}
+            />
             <Card.Body>
               <Card.Title>Silvana</Card.Title>
               <Card.Text>
@@ -55,6 +72,28 @@ const About = () => {
             <Card.Img className={classes.cardImage} variant="top" src={daniel} />
             <Card.Body>
               <Card.Title>Daniel</Card.Title>
+              <Card.Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+                nec fringilla enim. Cras sodales ante sed urna varius congue.
+                Mauris commodo turpis metus, sed mattis urna blandit at. Cras
+                sit amet erat pretium, commodo urna ac, accumsan mi. Quisque non
+                diam metus. Curabitur nec elit a elit tempor molestie. Sed
+                ultricies egestas justo sit amet luctus.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={12} md={12} lg={12} className={classes.myCol}>
+          <Card className={classes.usCard} >
+            <Card.Img
+              className={classes.cardImage}
+              variant="top"
+              src={both}
+            />
+            <Card.Body>
+              <Card.Title>Us</Card.Title>
               <Card.Text>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
                 nec fringilla enim. Cras sodales ante sed urna varius congue.
