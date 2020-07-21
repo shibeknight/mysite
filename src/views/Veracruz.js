762e730aaad46689b491d73ca5d31580d0527d39
Daniel Antonio Ceballos Flores <danielc@linqstream.com>
2020-07-21 14:18:50 -0400

    Working on veracruz

--- src/views/Veracruz.js
+++ src/views/Veracruz.js
@@ -1,7 +1,43 @@
 import React from "react";
+import { createUseStyles } from "react-jss";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+
+const useStyles = createUseStyles({
+  myContainer: {
+    border: "solid",
+  },
+  myCol: {
+    display: "flex",
+    justifyContent: "center",
+    paddingTop: '5vh'
+  },
+  myImage: {
+    maxHeight: "60vh",
+  },
+  myButton: {
+    maxWidth: '40vw'
+  }
+});
 
 const Veracruz = () => {
-  return <div>Veracruz page</div>;
+  const classes = useStyles();
+  return (
+    <Container className={classes.myContainer}>
+      <Row>
+        <Col className={classes.myCol}>
+          <Image
+            className={classes.myImage}
+            src="https://i.redd.it/kga8zxvvmh751.jpg"
+            rounded
+            fluid
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Veracruz;
