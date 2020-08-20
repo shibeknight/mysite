a01d824ae3a707bfd37bd22608f5e6066c6e639f
Daniel Antonio Ceballos Flores <danielc@linqstream.com>
2020-08-20 19:19:58 -0400

    Fix scroll to top

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
+import ScrollToTop from "./utils/ScrollToTop";
 
 ReactDOM.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>,
   document.getElementById("root")
